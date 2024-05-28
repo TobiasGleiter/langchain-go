66420d196c3dc048ecf0387d2f08662115ec256d
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-05-28 07:09:51 +0200

    chore: output string parser

--- examples/output/strings/main.go
+++ examples/output/strings/main.go
@@ -7,15 +7,15 @@ import (
 )
 
 func main() {
-	stringOutputParser := output.StringOutputParser{}
-	parsedStringOutput, _ := stringOutputParser.Parse("hi, bye")
+	var parser output.OutputParser[string] = &output.StringOutputParser{}
+	parsedStringOutput, _ := parser.Parse("hi, bye")
 	fmt.Println(parsedStringOutput)
 
-	spaceSeparatedListOutputParser := output.SpaceSeparatedListOutputParser{}
-	parsedSpaceSeparatedListOutput, _ := spaceSeparatedListOutputParser.Parse("hi, bye")
-	fmt.Println(parsedSpaceSeparatedListOutput)
+	var spaceParser output.OutputParser[[]string] = &output.SpaceSeparatedListOutputParser{}
+    spaceResult, _ := spaceParser.Parse("example output with spaces")
+    fmt.Println(spaceResult)
 
-	commaSeparatedListOutputParser := output.CommaSeparatedListOutputParser{}
-	parsedCommaSeparatedListOutput, _ := commaSeparatedListOutputParser.Parse("hi, bye")
-	fmt.Println(parsedCommaSeparatedListOutput)
+    var commaParser output.OutputParser[[]string] = &output.CommaSeparatedListOutputParser{}
+    commaResult, _ := commaParser.Parse("example,output,with,commas")
+    fmt.Println(commaResult)
 }
--- model_io/output/strings_parser.go
+++ model_io/output/strings_parser.go
@@ -1,21 +1,68 @@
+// package output
+
+// import (
+// 	"strings"
+// )
+
+// type StringOutputParser struct {}
+// type SpaceSeparatedListOutputParser struct {}
+// type CommaSeparatedListOutputParser struct {}
+
+// func (p *StringOutputParser) Parse(output string) (string, error) {
+//     return output, nil
+// }
+
+// func (p *CommaSeparatedListOutputParser) Parse(output string) ([]string, error) {
+//     return strings.Split(output, ","), nil
+// }
+
+// func (p *SpaceSeparatedListOutputParser) Parse(output string) ([]string, error) {
+//     return strings.Split(output, " "), nil
+// }
+
 package output
 
 import (
 	"strings"
 )
 
-type StringOutputParser struct {}
-type SpaceSeparatedListOutputParser struct {}
-type CommaSeparatedListOutputParser struct {}
+// Implemented String Parsers
+type StringOutputParser struct{}
+type SpaceSeparatedListOutputParser struct{}
+type CommaSeparatedListOutputParser struct{}
 
 func (p *StringOutputParser) Parse(output string) (string, error) {
-    return output, nil
+	return output, nil
 }
 
-func (p *CommaSeparatedListOutputParser) Parse(output string) ([]string, error) {
-    return strings.Split(output, ","), nil
+func (p *StringOutputParser) ParseWithPrompt(output string, prompt PromptValue) (string, error) {
+	return output, nil
+}
+
+func (p *StringOutputParser) GetFormatInstructions() string {
+	return "Return the output as a plain string."
 }
 
 func (p *SpaceSeparatedListOutputParser) Parse(output string) ([]string, error) {
-    return strings.Split(output, " "), nil
+	return strings.Split(output, " "), nil
+}
+
+func (p *SpaceSeparatedListOutputParser) ParseWithPrompt(output string, prompt PromptValue) ([]string, error) {
+	return strings.Split(output, " "), nil
+}
+
+func (p *SpaceSeparatedListOutputParser) GetFormatInstructions() string {
+	return "Return the output as a space-separated list."
+}
+
+func (p *CommaSeparatedListOutputParser) Parse(output string) ([]string, error) {
+	return strings.Split(output, ","), nil
+}
+
+func (p *CommaSeparatedListOutputParser) ParseWithPrompt(output string, prompt PromptValue) ([]string, error) {
+	return strings.Split(output, ","), nil
+}
+
+func (p *CommaSeparatedListOutputParser) GetFormatInstructions() string {
+	return "Return the output as a comma-separated list."
 }
