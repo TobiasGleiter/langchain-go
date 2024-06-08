8854477d244dc02fd0a751275fa1264d7af59944
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-06-08 16:58:22 +0200

    chore: time agent

---
+++ examples/agents/time.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/TobiasGleiter/langchain-go/agents"
+	"github.com/TobiasGleiter/langchain-go/core/models/llms/ollama"
+)
+
+type GetTime struct {}
+
+func main() {
+	wizardlm2_7b := ollama.OllamaModel{
+		Model:  "wizardlm2:7b",
+		Options: ollama.ModelOptions{NumCtx: 4096},
+		Stream: false,
+		Stop:   []string{"\nObservation", "Observation"},
+	}
+	llm := ollama.NewOllamaClient(wizardlm2_7b)
+
+
+	tools := map[string]agents.Tool{
+		"GetTime": GetTime{},
+	}
+
+	timeAgent := agents.NewAgent(llm, tools)
+	timeAgent.Task("What time is it?")
+
+	ctx := context.TODO()
+	executor := agents.NewExecutor(*timeAgent)
+	executor.Run(ctx)
+}
+
+func (t GetTime) Name() string { return "GetTime" }
+
+func (t GetTime) Call(ctx context.Context, input string) (string, error) {
+	currentTime := time.Now()
+	return currentTime.String(), nil
+}
