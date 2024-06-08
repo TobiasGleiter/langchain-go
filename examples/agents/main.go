aa39a3b87354aaedcdc3cccf513515ad7a5437c7
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-06-08 08:35:14 +0200

    refactor: agents

--- examples/agents/main.go
+++ examples/agents/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"context"
-	"io/ioutil"
 	"strings"
 	"strconv"
 	"errors"
@@ -21,6 +19,7 @@ type SaveToFile struct {}
 
 func main() {
 	// Agent need a llm and tools
+	// Agent Executor iterates 10 timees that the agent can solve the task.
 	// Currently the agent works with the ReAct Prompt Pattern
 	wizardlm2_7b := ollama.OllamaModel{
 		Model:  "wizardlm2:7b", // This is the best working model, currently.
@@ -31,34 +30,21 @@ func main() {
 	llm := ollama.NewOllamaClient(wizardlm2_7b)
 
 	tools := map[string]agents.Tool{
-		"CurrentDatetime": CurrentDatetime{},
 		"CurrentTemperatureInFahrenheit": CurrentTemperatureInFahrenheit{},
 		"FormatFahrenheitToCelsius": FormatFahrenheitToCelsius{},
-		"SaveToFile": SaveToFile{},
 	}
 
 	agent := agents.NewAgent(llm, tools)
 	agent.Task("How is the temperature in celsius?")
-	//agent.Task("What time is it?")
-	
+
 	ctx := context.TODO()
 	executor := agents.NewExecutor(*agent)
 	executor.Run(ctx)
 
-
 }
 
-// Implementation of the Tools
-func (t CurrentDatetime) Name() string {
-	return "CurrentDatetime"
-}
-
-func (t CurrentDatetime) Call(ctx context.Context, input string) (string, error) {
-	now := time.Now()
-	formattedTime := now.Format(time.ANSIC)
-	return fmt.Sprintf("Current datetime: %s", formattedTime), nil
-}
-
+// Implementation of the tools!
+// Tools follow the tools interface with Name and Call.
 func (t CurrentTemperatureInFahrenheit) Name() string {
 	return "CurrentTemperatureInFahrenheit"
 }
@@ -74,31 +60,13 @@ func (t FormatFahrenheitToCelsius) Name() string {
 func (t FormatFahrenheitToCelsius) Call(ctx context.Context, input string) (string, error) {
 	fahrenheit, err := parseFahrenheit(input)
 	if err != nil {
+		// This need to be given by the user as an extra description that the model can reflect itself.
 		return "", errors.New("I should use this tool again using the temperature in this format: XX°F and pass it to the Action Input:")
 	}
 
-	// Convert Fahrenheit to Celsius
 	celsius := fahrenheitToCelsius(fahrenheit)
 
-	// Return the formatted string
 	return fmt.Sprintf("Current temperature: %.2f°C", celsius), nil
-}
-
-func (t SaveToFile) Name() string {
-	return "SaveToFile"
-}
-
-func (t SaveToFile) Call(ctx context.Context, input string) (string, error) {
-	// Save file logic here...
-	// The tool can also be an agent (e.g. for formatting the output)
-	data := []byte(input)
-    err := ioutil.WriteFile("example.txt", data, 0644)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return "Error saving the file.", nil 
-	}
-
-	return "File saved.", nil
 }
 
 func parseFahrenheit(input string) (float64, error) {
