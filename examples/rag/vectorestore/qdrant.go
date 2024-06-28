b74fea732fd2a81a7ecf20fcff1a8cdb2cd392da
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-06-27 21:00:13 -0400

    qdrant preperation

---
+++ examples/rag/vectorestore/qdrant.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+
+	"github.com/TobiasGleiter/langchain-go/core/embedder"
+	"github.com/TobiasGleiter/langchain-go/core/models/llms/ollama"
+	"github.com/TobiasGleiter/langchain-go/rag/vectorstore/qdrant"
+)
+
+func main() {
+	llama3_8b_model := ollama.OllamaModel{
+		Model:   "llama3:8b",
+		Options: ollama.ModelOptions{NumCtx: 4096},
+		Stream:  false,
+	}
+
+	ollamaClient := ollama.NewOllamaClient(llama3_8b_model)
+	ollamaEmbedder := embedder.NewEmbedder(ollamaClient)
+
+	qdrantUrl, _ := url.Parse("http://localhost:6333")
+	qdrant := qdrant.New(*ollamaEmbedder, "test", *qdrantUrl)
+
+	ctx := context.Background()
+	res, _ := qdrant.SimilaritySearch(ctx, "I don't think...")
+	fmt.Println(res)
+}
--- rag/vectorstore/qdrant/qdrant.go
+++ rag/vectorstore/qdrant/qdrant.go
@@ -8,30 +8,38 @@ import (
 	"github.com/TobiasGleiter/langchain-go/rag/vectorstore"
 )
 
-// 1. Qdrant Client
-// 2. Create a collection
-// 3. Add Vectors
-// 4. Run a query
-// 5. Add a filter
-
 type QdrantStore struct {
-	Embedder       embedder.Embedder
-	collectionName string
-	qdrantURL      url.URL
-	apiKey         string
-	contentKey     string
+	Embedder   embedder.Embedder
+	Collection string
+	Url        url.URL
+	apiKey     string
+	contentKey string
 }
 
-func New(embedder embedder.Embedder) *QdrantStore {
+func New(embedder embedder.Embedder, collection string, url url.URL) *QdrantStore {
 	return &QdrantStore{
-		Embedder: embedder,
+		Embedder:   embedder,
+		Collection: collection,
+		Url:        url,
 	}
 }
 
 func (qs *QdrantStore) AddDocuments(ctx context.Context, docs []vectorstore.Document) ([]string, error) {
+	// 1. Create an array of documents
+
+	// 2. Create go routine for each index of the array and run embedding concurrently
+
+	// 3. Add metadata (what is the metadata?)
+
+	// 4. Upsert points into qdrant vectorstore
+
 	return []string{}, nil
 }
 
-func (qs *QdrantStore) SimilaritySearch(ctx context.Context, query string, numberOfDocsToRetrun int) ([]vectorstore.Document, error) {
+func (qs *QdrantStore) SimilaritySearch(ctx context.Context, query string) ([]vectorstore.Document, error) {
+	// 1. Embed query
+
+	// 2. Search similar points in vectorstore
+
 	return []vectorstore.Document{}, nil
 }
