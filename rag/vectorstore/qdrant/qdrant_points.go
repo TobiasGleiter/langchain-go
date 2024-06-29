6ba6d2d5991405d941447485a1e36ab82af0d723
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-06-29 05:13:35 -0400

    feat: upsert delete count points

--- rag/vectorstore/qdrant/qdrant_points.go
+++ rag/vectorstore/qdrant/qdrant_points.go
@@ -1,65 +1,111 @@
 package qdrant
 
-import "context"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
-type UpsertPointsResponse struct {
-	Time   float32            `json:"time"`
-	Status string             `json:"status"`
-	Result UpsertPointsResult `json:"result"`
+func (qs *QdrantStore) upsertPoints(ctx context.Context, payload UpsertPointsRequest) (UpsertPointsResponse, error) {
+	upsertPoints, err := json.Marshal(payload)
+	if err != nil {
+		return UpsertPointsResponse{}, fmt.Errorf("failed to marshal request payload: %w", err)
+	}
+
+	upsertPointsUrl := qs.Url.String() + "/collections/" + qs.Collection + "/points"
+	upserPointsRequest, err := http.NewRequestWithContext(ctx, http.MethodPut, upsertPointsUrl, bytes.NewBuffer(upsertPoints))
+	if err != nil {
+		return UpsertPointsResponse{}, fmt.Errorf("failed to create HTTP request: %w", err)
+	}
+	upserPointsRequest.Header.Set("Authorization", "Bearer "+qs.apiKey)
+	upserPointsRequest.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	upsertPointsResponse, err := client.Do(upserPointsRequest)
+	if err != nil {
+		return UpsertPointsResponse{}, fmt.Errorf("failed to send HTTP request: %w", err)
+	}
+	defer upsertPointsResponse.Body.Close()
+
+	if upsertPointsResponse.StatusCode != http.StatusOK {
+		return UpsertPointsResponse{}, fmt.Errorf("unexpected status code: %d", upsertPointsResponse.StatusCode)
+	}
+
+	var upsertResponse UpsertPointsResponse
+	err = json.NewDecoder(upsertPointsResponse.Body).Decode(&upsertResponse)
+	if err != nil {
+		return UpsertPointsResponse{}, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return upsertResponse, nil
 }
 
-type DeletePointsResult = UpsertPointsResponse
+func (qs *QdrantStore) deletePoints(ctx context.Context, payload []DeletePointsRequest) (DeletePointsResponse, error) {
+	deletePoints, err := json.Marshal(payload)
+	if err != nil {
+		return DeletePointsResponse{}, fmt.Errorf("failed to marshal request payload: %w", err)
+	}
 
-type UpsertPointsResult struct {
-	Status      string `json:"status"`
-	OperationId int    `json:"operation_id"`
+	deletPointsUrl := qs.Url.String() + "/collections/" + qs.Collection + "/points/vectors/delete"
+	deletePointsRequest, err := http.NewRequestWithContext(ctx, http.MethodPost, deletPointsUrl, bytes.NewBuffer(deletePoints))
+	if err != nil {
+		return DeletePointsResponse{}, fmt.Errorf("failed to create HTTP request: %w", err)
+	}
+	deletePointsRequest.Header.Set("Authorization", "Bearer "+qs.apiKey)
+	deletePointsRequest.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	deletePointsResponse, err := client.Do(deletePointsRequest)
+	if err != nil {
+		return DeletePointsResponse{}, fmt.Errorf("failed to send HTTP request: %w", err)
+	}
+	defer deletePointsResponse.Body.Close()
+
+	if deletePointsResponse.StatusCode != http.StatusOK {
+		return DeletePointsResponse{}, fmt.Errorf("unexpected status code: %d", deletePointsResponse.StatusCode)
+	}
+
+	var deleteResponse DeletePointsResponse
+	err = json.NewDecoder(deletePointsResponse.Body).Decode(&deleteResponse)
+	if err != nil {
+		return DeletePointsResponse{}, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return deleteResponse, nil
 }
 
-func (qs *QdrantStore) upsertPoints(ctx context.Context, vectors [][]float32) (UpsertPointsResponse, error) {
-	// PUT /collections/{collection_name}/points
-	// {
-	// 	"batch": {
-	// 		"ids": [1, 2, 3],
-	// 		"payloads": [
-	// 			{"color": "red"},
-	// 			{"color": "green"},
-	// 			{"color": "blue"}
-	// 		],
-	// 		"vectors": [
-	// 			[0.9, 0.1, 0.1],
-	// 			[0.1, 0.9, 0.1],
-	// 			[0.1, 0.1, 0.9]
-	// 		]
-	// 	}
-	// }
-	return UpsertPointsResponse{}, nil
-}
+func (qs *QdrantStore) countPoints(ctx context.Context, payload CountPointsRequest) (CountPointsResponse, error) {
+	countPoints, err := json.Marshal(payload)
+	if err != nil {
+		return CountPointsResponse{}, fmt.Errorf("failed to marshal request payload: %w", err)
+	}
 
-func (qs *QdrantStore) deletePoints(ctx context.Context, ids []any) (DeletePointsResult, error) {
-	//	POST /collections/{collection_name}/points/vectors/delete
-	//
-	//	{
-	//	    "points": [0, 3, 100],
-	//	    "vectors": ["text", "image"]
-	//	}
-	return DeletePointsResult{}, nil
-}
+	countPointsUrl := qs.Url.String() + "/collections/" + qs.Collection + "/points/count"
+	countPointsRequest, err := http.NewRequestWithContext(ctx, http.MethodPost, countPointsUrl, bytes.NewBuffer(countPoints))
+	if err != nil {
+		return CountPointsResponse{}, fmt.Errorf("failed to create HTTP request: %w", err)
+	}
+	countPointsRequest.Header.Set("Authorization", "Bearer "+qs.apiKey)
+	countPointsRequest.Header.Set("Content-Type", "application/json")
 
-func (qs *QdrantStore) countPoints() (int, error) {
-	//	POST /collections/{collection_name}/points/count
-	//
-	//	{
-	//	    "filter": {
-	//	        "must": [
-	//	            {
-	//	                "key": "color",
-	//	                "match": {
-	//	                    "value": "red"
-	//	                }
-	//	            }
-	//	        ]
-	//	    },
-	//	    "exact": true
-	//	}
-	return 0, nil
+	client := &http.Client{}
+	countPointsResponse, err := client.Do(countPointsRequest)
+	if err != nil {
+		return CountPointsResponse{}, fmt.Errorf("failed to send HTTP request: %w", err)
+	}
+	defer countPointsResponse.Body.Close()
+
+	if countPointsResponse.StatusCode != http.StatusOK {
+		return CountPointsResponse{}, fmt.Errorf("unexpected status code: %d", countPointsResponse.StatusCode)
+	}
+
+	var countResponse CountPointsResponse
+	err = json.NewDecoder(countPointsResponse.Body).Decode(&countResponse)
+	if err != nil {
+		return CountPointsResponse{}, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return countResponse, nil
 }
--- rag/vectorstore/qdrant/qdrant_types.go
+++ rag/vectorstore/qdrant/qdrant_types.go
@@ -1,1 +1,38 @@
 package qdrant
+
+type ID string
+
+type UpsertPointsResponse struct {
+	Time   float32            `json:"time"`
+	Status string             `json:"status"`
+	Result UpsertPointsResult `json:"result"`
+}
+
+type DeletePointsResponse = UpsertPointsResponse
+
+type UpsertPointsResult struct {
+	Status      string `json:"status"`
+	OperationId int    `json:"operation_id"`
+}
+
+type UpsertPointsRequest struct {
+	Batch struct {
+		IDs      []ID                `json:"ids"`
+		Payloads []map[string]string `json:"payloads"`
+		Vectors  [][]float32         `json:"vectors"`
+	} `json:"batch"`
+}
+
+type DeletePointsRequest struct {
+	Points []ID
+}
+
+type CountPointsRequest struct{}
+
+type CountPointsResponse struct {
+	Time   float32 `json:"time"`
+	Status string  `json:"status"`
+	Result struct {
+		Count int `json:"count"`
+	} `json:"result"`
+}
